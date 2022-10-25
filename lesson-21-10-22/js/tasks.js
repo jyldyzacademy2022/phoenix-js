b13fd3c54206fdeaaf9cd63d56ef7c4986daa76e
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2022-10-25 20:04:33 +0600

    add lesson october 21

---
+++ lesson-21-10-22/js/tasks.js
@@ -0,0 +1,38 @@
+// 1. Создайте переменную a = 6, b = 3. Создайте в разметке div с классом result и в этот div выведите результат умножения
+
+// 2. Создайте num1 = 3, num2 = 2 Создайте в разметке div с классом result и в этот div выведите результат деления num1 на num2
+
+// 3. Создайте num1 = 50, num2 = 2 Создайте в разметке div с классом result и в этот div выведите остаток деления num1 на num2
+
+// let a = 50;
+// let b = 4;
+
+// let result = a ** b;
+
+// const output = document.querySelector(".result");
+// output.innerText = result;
+
+// let result = num1 + num2;
+
+// const output = document.querySelector(".result");
+// output.innerText = result;
+
+/*
+function getSum(a, b) {
+  let num1 = document.querySelector("#number-1");
+  let num2 = document.querySelector("#number-2");
+
+  num1 = +num1.value;
+  num2 = +num2.value;
+
+  let result = num1 + num2;
+
+  const output = document.querySelector(".result");
+  output.innerText = result;
+}
+
+const btn = document.querySelector("#calculate");
+
+btn.addEventListener("click", getSum);
+
+*/
