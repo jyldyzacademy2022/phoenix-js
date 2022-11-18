87ea08048ff3c8d439c8e6f84cf22bc2ab4c2c48
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2022-11-18 20:55:16 +0600

    add lesson 15-11

---
+++ lesson-15-11-22/script.js
@@ -0,0 +1,41 @@
+// Math.random()
+// Math.max()
+// Math.min()
+// Math.floor()
+// Math.ceil()
+// Math.round()
+
+// Number.toFixed()
+
+// function formatUserName(s) {
+//   s = s.trim();
+//   return s[0].toUpperCase() + s.substring(1).toLowerCase();
+// }
+
+// let formattedInput = formatUserName(userInput);
+
+// document.write(`<h1>${formattedInput}</h1>`);
+
+// let formattedInput = J +
+// 01234
+
+// myString = myString[0].toUpperCase();
+
+// console.log(myString.toLowerCase());
+
+// var anyString = "Mozilla";
+// 0123456;
+
+// // Отобразит 'Moz'
+// // console.log(anyString.substring(0, 3));
+// console.log(anyString.substring(1));
+
+// let userInput = "vasyavadsav";
+
+// console.log(userInput.replaceAll("v", "V"));
+
+let myFriends = ["Masha", "Vasya", "Pete"];
+
+for (let i = 0; i < myFriends.length; i++) {
+  console.log(myFriends[i]);
+}
