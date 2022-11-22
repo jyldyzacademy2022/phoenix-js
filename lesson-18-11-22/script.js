771f4cddfefdb902f2b7312d077eb508d01f0f58
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2022-11-22 20:56:48 +0600

    add lesson 18-11-22

---
+++ lesson-18-11-22/script.js
@@ -0,0 +1,110 @@
+// let friends = ["john", "vasya", "pete"];
+// console.log(friends);
+// // friends[104] = "anna";
+// // console.log(friends);
+
+// friends.push("anna");
+// friends.push("mary");
+// friends.unshift("akmat");
+
+// console.log(friends);
+
+// let deletedFriend = friends.pop();
+// console.log(friends);
+// console.log(`Good bye ${deletedFriend}`);
+
+// let deletedFriend2 = friends.shift();
+// console.log(friends);
+// console.log(`Good bye ${deletedFriend2}`);
+
+// delete friends[2];
+
+// let enemies = ["alex", "misha", "bob"];
+
+// let friendAndEmemies = friends.concat(enemies);
+
+// for (let i = 0; i < friends.length; i++) {
+//   console.log(`Hello ${friends[i]}`);
+// }
+
+// for (let i = 0; i < enemies.length; i++) {
+//   console.log(`Hello ${enemies[i]}`);
+// }
+
+// let enemies = ["alex", "misha", "bob"];
+
+// function greet(name) {
+//   console.log(`Hello ${name}`);
+// }
+
+// friends.forEach(greet);
+// enemies.forEach(greet);
+// friends.forEach((item) => console.log(`Hello ${item}`));
+// enemies.forEach((item) => console.log(`Hello ${item}`));
+
+// if (friends.includes("vasya")) {
+//   console.log("У тебя есть друг по имени Вася");
+// } else {
+//   console.log("У тебя нет такого друга");
+// }
+
+// console.log(friends);
+
+// let sortedFriends = friends.sort();
+
+// console.log(sortedFriends);
+
+// console.log(friends);
+
+// let user = ["вася", "пупкин"];
+
+// let [firstName, lastName] = user;
+
+// let firstName = user[0];
+// let lastName = user[1];
+
+// console.log(firstName, lastName);
+
+// let user = {
+//   name: "vasya",
+//   surname: "pupkin",
+// };
+
+// // let firstName = user.firstName;
+// // let lastName = user.lastName;
+
+// let { name, surname } = user;
+
+// console.log(name, surname);
+
+// let user = ["вася", "пупкин"];
+
+// let [lastName, firstName] = user;
+
+// console.log(lastName, firstName);
+
+// let today = new Date();
+// let date = today.getDate();
+// let day = today.getDay();
+// let month = today.getMonth();
+
+// console.log(today);
+// console.log(date);
+// console.log(day);
+
+// console.log(month);
+
+// 0 - вос
+// 1 - пн
+
+// 0 - январь
+
+// let today = new Date();
+// console.log(today);
+// today.setDate(15);
+// today.setFullYear(2045);
+// console.log(today);
+
+// let tomorrow = new Date(2025, 5);
+
+// console.log(tomorrow);
